eaf9c9c2ede337c9fa881c810de23821a9535955
Camilo XPS 13 <camilo.salazar@udea.edu.co>
2018-12-28 14:10:57 -0500

    add root initial config and change the MG folder name

--- LocalConfig.sh
+++ LocalConfig.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-export MadGrapgSYS=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_2_3_Root5
+source /home/camilo/HEPTools/MadGraph/MG5_aMC_v2_5_4_Root-6/ThisMadGraph6.sh
+
+export MadGrapgSYS=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_5_5_Root6
 export PATH=$PATH:$MadGrapgSYS/bin
