b8c8e617ec4f05d8fc926c74a669bc000813a057
Camilo Andrés Salazar Gonzales <camilo@gfif.udea.edu.co>
2018-12-28 14:03:53 -0500

    change to MG5_aMC_v2_5_5

--- LocalConfig.sh
+++ LocalConfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export MadGrapgSYS=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_2_3_Root5
+export MadGrapgSYS=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_5_5
 export PATH=$PATH:$MadGrapgSYS/bin
