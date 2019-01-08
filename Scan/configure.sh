ed33f6cb2c2f36e1f938e1f2dfdbf73dcd54fb9c
Camilo XPS 13 <camilo.salazar@udea.edu.co>
2019-01-08 16:35:37 -0500

    Scan progran made by Carlos

---
+++ Scan/configure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/camilo/HEPTools/ROOT/root/Root6Python2_7/bin/thisroot.sh
+
+export LD_LIBRARY_PATH=/home/camilo/HEPTools/DELPHES/Delphes_3_2_0_R6:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_5_5_Root6/ExRootAnalysis:$LD_LIBRARY_PATH
