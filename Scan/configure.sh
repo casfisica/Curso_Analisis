4766389e8afca17d55a207254942a5353fb714a8
Camilo XPS 13 <camilo.salazar@udea.edu.co>
2019-01-10 12:12:07 -0500

    Some updates

--- Scan/configure.sh
+++ Scan/configure.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 source /home/camilo/HEPTools/ROOT/root/Root6Python2_7/bin/thisroot.sh
 
-export LD_LIBRARY_PATH=/home/camilo/HEPTools/DELPHES/Delphes_3_2_0_R6:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_5_5_Root6/ExRootAnalysis:$LD_LIBRARY_PATH
+
+MadGraphPath=/home/camilo/HEPTools/MADGRAPH/MG5_aMC_v2_5_5_Root6
+DelphesPath=$MadGraphPath/Delphes
+
+export LD_LIBRARY_PATH=$DelphesPath:$LD_LIBRARY_PATH
+
+ExRootAnalysisPath=
+export LD_LIBRARY_PATH=$MadGraphPath/ExRootAnalysis:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$MadGraphPath/ExRootAnalysis/ExRootAnalysis:$LD_LIBRARY_PATH
+
+#To root reconize the the delphes library
+
+export ROOT_INCLUDE_PATH=$DelphesPath:$ROOT_INCLUDE_PATH
+export ROOT_INCLUDE_PATH=$DelphesPath/ExRootAnalysis:$ROOT_INCLUDE_PATH
